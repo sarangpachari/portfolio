d31af6eaa03d02d845a4b22de14d5b30d08b6795
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2024-09-06 17:56:22 +0530

    initial-commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/profile.png'
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
   return (
-    <div id='home' className='hero'>
+    <div id="home" className="hero">
       <img src={profile_img} alt="" />
-      <h1><span>I'm Sarang, </span>MERN Stack Developer</h1>
-      <p>Seeking Opportunities in Software Development | Proficient in Full Stack Development | Eager to Contribute to a Dynamic Team</p>
+      <h1>
+        <span>I'm Sarang, </span>MERN Stack Developer
+      </h1>
+      <p>
+        I'm a self-taught MERN stack web developer with a BCA degree and a
+        strong passion for building dynamic and user-friendly web applications.
+        
+      </p>
       <div className="hero-action">
-        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-        <a className='resume-a' href="https://drive.google.com/file/d/1wSFEbisR4bP1hP8CXiXtJA6HfcUXqhgx/view?usp=sharing"><div className="hero-resume">My resume</div></a>
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me
+          </AnchorLink>
+        </div>
+        <a
+          className="resume-a"
+          href="https://drive.google.com/file/d/1pp7uW4QcEx0uPXpSByG3dv5hTT6q5Q1C/view?usp=sharing"
+        >
+          <div className="hero-resume">My resume</div>
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -1,32 +1,108 @@
-import React from 'react'
-import './MyWork.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
-import project_1 from '../../assets/project_1.png'
-import project_2 from '../../assets/project_2.png'
-import project_3 from '../../assets/project_3.png'
-import project_4 from '../../assets/project_4.png'
-import arrow_icon from '../../assets/arrow_icon.svg'
-
+import React from "react";
+import "./MyWork.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import project_1 from "../../assets/project-1.png";
+import project_2 from "../../assets/project-2.png";
+import project_3 from "../../assets/project-3.png";
+import project_4 from "../../assets/project-4.png";
+import project_5 from "../../assets/project-5.png";
+import project_6 from "../../assets/project-6.png";
+import project_7 from "../../assets/project-7.png";
+import project_8 from "../../assets/project-8.png";
+import project_9 from "../../assets/project-9.png";
+import project_10 from "../../assets/project-10.png";
+import project_11 from "../../assets/project-11.png";
+import arrow_icon from "../../assets/arrow_icon.svg";
 
 const MyWork = () => {
   return (
-    <div id='work' className='mywork'>
-        <div className="mywork-title">
-            <h1>My latest projects</h1>
-            <img src={theme_pattern} alt="" />
-        </div>
-        <div className="mywork-container">
+    <div id="work" className="mywork">
+      <div className="mywork-title">
+        <h1>My projects</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="mywork-container">
+        <a href="https://qbucket.mac.edu.in/">
+          <div>
             <img src={project_1} alt="" />
-            <img src={project_2} alt="" />
-            <img src={project_3} alt="" />
-            <img src={project_4} alt="" />
-        </div>
-        <div className="mywork-showmore">
-            <p>Show More</p>
-            <img src={arrow_icon} alt="" />
-        </div>
-    </div>
-  )
-}
+            <p>Q-Bucket</p>
+          </div>
+        </a>
 
-export default MyWork
+        <div>
+          <img src={project_2} alt="" />
+          <p>E-Commerce</p>
+        </div>
+
+        <a href="https://simple-clone-by-sarang.netlify.app/">
+          <div>
+            <img src={project_3} alt="" />
+            <p>Microsoft Clone</p>
+          </div>
+        </a>
+
+        <a href="https://sarangpachari.github.io/todoapp/">
+          <div>
+            <img src={project_4} alt="" />
+            <p>Todo App</p>
+          </div>
+        </a>
+
+        <a href="https://justhomebysarang.netlify.app/#">
+          <div>
+            <img src={project_5} alt="" />
+            <p>Just Home</p>
+          </div>
+        </a>
+
+        <a href="https://sarangpachari.github.io/macmap/">
+          <div>
+            <img src={project_6} alt="" />
+            <p>MA College Map</p>
+          </div>
+        </a>
+
+        <a href="https://meplants.netlify.app/">
+          <div>
+            <img src={project_7} alt="" />
+            <p>Me plants</p>
+          </div>
+        </a>
+
+        <a href="https://sarangpachari.github.io/furnitureworld/">
+          <div>
+            <img src={project_8} alt="" />
+            <p>Furniture World</p>
+          </div>
+        </a>
+
+        <a href="https://foodhut-sarang.netlify.app/">
+          <div>
+            <img src={project_9} alt="" />
+            <p>Food Hut</p>
+          </div>
+        </a>
+
+
+          <div>
+            <img src={project_10} alt="" />
+            <p>Digi Expert</p>
+          </div>
+
+
+  
+          <div>
+            <img src={project_11} alt="" />
+            <p>Books Mart</p>
+          </div>
+
+      </div>
+      <div className="mywork-showmore">
+        <p>Show More</p>
+        <img src={arrow_icon} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default MyWork;
